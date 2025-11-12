809009f71ae2dd59fb9502bbe1c53a0cc652511a
MNedkov09 <martin.nedkov2009@gmail.com>
2025-11-12 17:44:21 +0200

    Homework 2

--- RayTracing_2/DirectX/main.cpp
+++ RayTracing_2/DirectX/main.cpp
@@ -1,48 +1,93 @@
 #include <iostream>
-#include <dxgi.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
 #pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d12.lib")
 
 int main()
 {
-    IDXGIFactory *pFactory = nullptr;
-    if (FAILED(CreateDXGIFactory(__uuidof(IDXGIFactory), (void **)&pFactory)))
+    IDXGIFactory4 *pFactory = nullptr;
+    if (FAILED(CreateDXGIFactory1(IID_PPV_ARGS(&pFactory))))
     {
         std::cout << "Failed to create DXGIFactory\n";
         return -1;
     }
 
-    IDXGIAdapter *pAdapter = nullptr;
-    UINT index = 0;
-    while (pFactory->EnumAdapters(index, &pAdapter) != DXGI_ERROR_NOT_FOUND)
+    IDXGIAdapter1 *pAdapter = nullptr;
+    for (UINT i = 0; DXGI_ERROR_NOT_FOUND != pFactory->EnumAdapters1(i, &pAdapter); ++i)
     {
-        DXGI_ADAPTER_DESC desc;
-        pAdapter->GetDesc(&desc);
+        DXGI_ADAPTER_DESC1 desc;
+        pAdapter->GetDesc1(&desc);
 
-        std::wcout << L"Adapter " << index << L": " << desc.Description << std::endl;
+        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+            continue;
 
-        switch (desc.VendorId)
+        if (SUCCEEDED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
         {
-        case 0x10DE:
-            std::wcout << L"Vendor: NVIDIA" << std::endl;
-            break;
-        case 0x1002:
-            std::wcout << L"Vendor: AMD" << std::endl;
-            break;
-        case 0x8086:
-            std::wcout << L"Vendor: Intel" << std::endl;
-            break;
-        default:
-            std::wcout << L"Vendor: Unknown (0x" << std::hex << desc.VendorId << L")" << std::endl;
+            std::wcout << L"Using adapter: " << desc.Description << std::endl;
             break;
         }
-
-        std::wcout << L"Dedicated Video Memory: " << (desc.DedicatedVideoMemory / (1024 * 1024)) << L" MB" << std::endl;
-        std::wcout << std::endl;
-
         pAdapter->Release();
-        index++;
     }
 
+    if (!pAdapter)
+    {
+        std::cout << "No suitable GPU found\n";
+        pFactory->Release();
+        return -1;
+    }
+
+    ID3D12Device *pDevice = nullptr;
+    if (FAILED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&pDevice))))
+    {
+        std::cout << "Failed to create D3D12 device\n";
+        pAdapter->Release();
+        pFactory->Release();
+        return -1;
+    }
+
+    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+    queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    ID3D12CommandQueue *pCommandQueue = nullptr;
+    if (FAILED(pDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&pCommandQueue))))
+    {
+        std::cout << "Failed to create Command Queue\n";
+        pDevice->Release();
+        pAdapter->Release();
+        pFactory->Release();
+        return -1;
+    }
+
+    ID3D12CommandAllocator *pCommandAllocator = nullptr;
+    if (FAILED(pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&pCommandAllocator))))
+    {
+        std::cout << "Failed to create Command Allocator\n";
+        pCommandQueue->Release();
+        pDevice->Release();
+        pAdapter->Release();
+        pFactory->Release();
+        return -1;
+    }
+
+    ID3D12GraphicsCommandList *pCommandList = nullptr;
+    if (FAILED(pDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, pCommandAllocator, nullptr, IID_PPV_ARGS(&pCommandList))))
+    {
+        std::cout << "Failed to create Command List\n";
+        pCommandAllocator->Release();
+        pCommandQueue->Release();
+        pDevice->Release();
+        pAdapter->Release();
+        pFactory->Release();
+        return -1;
+    }
+
+    std::cout << "Direct3D 12 initialization successful\n";
+
+    pCommandList->Release();
+    pCommandAllocator->Release();
+    pCommandQueue->Release();
+    pDevice->Release();
+    pAdapter->Release();
     pFactory->Release();
     return 0;
 }
